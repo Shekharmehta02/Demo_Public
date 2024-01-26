37dcc784fcca50ba7ca8d654df97b53f476bf39d
[Shekhar Mehta] <[Shekharmehta643@gmail.com]>
2024-01-26 11:19:54 +0530

    V1.6

---
+++ Offer_eligible_Customers.py
@@ -0,0 +1,11 @@
+This file contains list of customer who are eligible for offer
+Line 11
+Line 22
+.
+.
+.
+
+.
+.
+.
+Line 1111
---
+++ Old_Customers.py
@@ -0,0 +1,15 @@
+This is old customer data file
+Line 1
+Line 2
+
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+Line - 1000
